83cf919d12585dce4a1e64ed8d9b4b0a594ded1e
John Serrano <andrey_201015@hotmail.com>
2023-08-18 11:15:27 -0500

    Add property read only

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,6 @@ import { Product } from './product.model';
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+  // data.id = 'johufeuhfeuf'
   products.push(data);
 }
