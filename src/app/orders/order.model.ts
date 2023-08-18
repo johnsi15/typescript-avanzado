30ae590322b8d5e6e74ba5ba1f46d15f42d2945e
John Serrano <andrey_201015@hotmail.com>
2023-08-18 11:11:51 -0500

    Add extends interfaces

--- src/app/orders/order.model.ts
+++ src/app/orders/order.model.ts
@@ -1,8 +1,8 @@
 import { Product } from './../products/product.model';
 import { User } from './../users/user.model';
+import { BaseModel } from '../base.model'
 
-export interface Order {
-  id: string | number;
+export interface Order extends BaseModel {
   createdAt: Date;
   products: Product[];
   user: User;
--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,8 +1,8 @@
 import { Category } from '../categories/category.model';
+import { BaseModel } from '../base.model'
 
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
-export interface Product {
-  id: string | number;
+export interface Product extends BaseModel {
   title: string;
   createdAt: Date;
   stock: number;
