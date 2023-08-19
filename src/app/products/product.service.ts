ab5c7a5fcddf25bbf1818da5f8965266c93da9fb
John Serrano <andrey_201015@hotmail.com>
2023-08-19 11:04:58 -0500

    Add Omit y Pick Type and/or dtos

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,4 +1,6 @@
+import { faker } from '@faker-js/faker';
 import { Product } from './product.model';
+import { CreateProductDto } from './product.dto'
 
 export const products: Product[] = [];
 
@@ -10,9 +12,24 @@ export const getProductById = (id: string) => {
   return products.find(product => product.id === id)
 }
 
-export const addProduct = (data: Product) => {
+export const addProduct = (data: CreateProductDto): Product => {
   // data.id = 'johufeuhfeuf'
-  products.push(data);
+  const { categoryId, ...newData } = {
+    ...data,
+    id: faker.string.uuid(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    category: {
+      id: faker.string.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    hola: 'hello world' // como curiosidad return una data de type Product pero me deja pasar una propiedad que no existe
+  }
+  products.push(newData);
+
+  return newData
 }
 
 export const updateProduct = (id: string, changes: Product) => {
