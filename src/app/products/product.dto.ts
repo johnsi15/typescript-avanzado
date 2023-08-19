0b71e2ee1670c291e65e2b5856a93e7efddabbe7
John Serrano <andrey_201015@hotmail.com>
2023-08-19 11:06:59 -0500

    Add Omit y Pick Type and/or dtos#2

--- src/app/products/product.dto.ts
+++ src/app/products/product.dto.ts
@@ -3,3 +3,6 @@ import { Product } from './product.model'
 export interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
   categoryId: string
 }
+
+// Es como el Omit pero acá solo le indicamos que campos son los que queremos
+type example = Pick<Product, 'color' | 'description' | 'title'>
