8c1bcefd8031e4fa90c010a54319d0681a665543
John Serrano <andrey_201015@hotmail.com>
2023-09-12 16:22:55 -0500

    Add constructor improved

---
+++ src/poo/05-constructor.ts
@@ -0,0 +1,60 @@
+export class MyDate {
+  // propiedades o atributos
+  readonly dbName: string;
+  #otherValue: string | null;
+
+  // constructor
+  constructor(
+    public year: number = 2023,
+    public month: number = 7,
+    private day: number = 10
+  ) {
+    this.dbName = 'hello';
+    this.#otherValue = null;
+  }
+
+  printFormat(): string {
+    const day = this.addZero(this.day);
+    const month = this.addZero(this.month);
+
+    return `${day}/${month}/${this.year}`;
+  }
+
+  private addZero(value: number) {
+    if (value < 10) {
+      return `0${value}`;
+    }
+
+    return value.toString();
+  }
+
+  public add(amount: number, type: 'days' | 'months' | 'years') {
+    // methods can also be public by default
+    if (type === 'days') {
+      this.day += amount;
+    } else if (type === 'months') {
+      this.month += amount;
+    } else {
+      this.year += amount;
+    }
+  }
+
+  getDay() {
+    console.log(this.#otherValue);
+    this.#methodPrivate();
+    return this.day;
+  }
+
+  #methodPrivate() {
+    console.log('Other method private');
+  }
+}
+
+const myDate = new MyDate();
+
+console.log(myDate.printFormat());
+
+const myDate2 = new MyDate(1992, 9);
+
+console.log(myDate2.printFormat());
+console.log(myDate2.getDay());
