cfc8fab3ceb05a526854d589eb0b663be2878959
John Serrano <andrey_201015@hotmail.com>
2023-09-30 09:51:15 -0500

    Add decoratos with class-validator

---
+++ src/poo/app/dtos/category.dto.ts
@@ -0,0 +1,34 @@
+import { IsDate, IsEnum, IsNotEmpty, IsUrl, Length, validate, validateOrReject } from 'class-validator'
+import { AccessType, Category } from '../models/category.model'
+
+export interface ICreateCategoryDto extends Omit<Category, 'id'> {}
+
+export class CreateCategoryDto implements ICreateCategoryDto {
+  @IsNotEmpty()
+  @Length(3, 255)
+  name!: string
+
+  @IsUrl()
+  @IsNotEmpty()
+  image!: string
+
+  @IsDate()
+  createdAt!: Date
+
+  @IsDate()
+  updatedAt!: Date
+
+  @IsEnum(AccessType)
+  access?: AccessType | undefined
+}
+
+;(async () => {
+  try {
+    const dto = new CreateCategoryDto()
+    dto.name = 'test'
+    dto.image = 'https://test.com'
+    await validateOrReject(dto)
+  } catch (error) {
+    console.log(error)
+  }
+})()
--- src/poo/app/models/category.model.ts
+++ src/poo/app/models/category.model.ts
@@ -1,7 +1,13 @@
+export enum AccessType {
+  PRIVATE = 'private',
+  PUBLIC = 'public',
+}
+
 export interface Category {
   id: string
   name: string
   image: string
   createdAt: Date
   updatedAt: Date
+  access?: AccessType
 }
